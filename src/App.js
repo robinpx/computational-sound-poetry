ad887fdfecf1af24291a73bf2cb40c971967a40b
robin <robin.rb.luo@gmail.com>
2022-03-23 09:16:57 +0800

    Add remaining React files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Index from './components/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div id="container">
+      <div id="content">
+      <Routes>
+        <Route index element={<Index />} />
+      </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
