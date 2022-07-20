4ddc9182b0155a560e20e7b16214b9a188b4f397
robin <robin.rb.luo@gmail.com>
2022-07-19 21:13:59 -0400

    Update index page

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -23,19 +23,23 @@ function Pages() {
   }
   else if (windowlink.includes("computational-sound-poetry") > 0) {
     return (
-      <div id="container-strict">
+      <div id="container-no-strict">
       <div id="cover">
       <header>
       <div id="title">Computational Sound Poetry</div>
       </header>
-      <div id="index">
-            <a href="?chaos-theory">chaos theory</a>
-            <a href="?pattern-recognition">pattern recognition</a>
-            <a href="?silent-forest">林中答案</a>
-            <a href="?eternal">eternal ear</a>
-      </div>
       <div id="desc"><i>Computational Sound Poetry</i> is a small collection of working interactive poems written by <a href="https://robinpx.github.io">Robin *</a> that designs spiritual textual landscapes about chaos and balance, silence and noise, peace and terror with sound and computation.</div>
-      </div></div>
+      <div id="index">
+        <ol>
+            <li><a href="?chaos-theory">Chaos Theory</a></li>
+            <li><a href="?pattern-recognition">Pattern Recognition</a></li>
+            <li><a href="?silent-forest">林中答案</a></li>
+            <li><a href="?eternal">Eternal Ear</a></li>
+        </ol>
+            
+      </div>
+      </div>
+      </div>
     );
   }
   return <React.Fragment/>
